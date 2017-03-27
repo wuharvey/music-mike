fa036e9a4980392608739e018e28aaa5f4b87b42
habdulkafi <habdulkafi@wesleyan.edu>
2017-03-26 21:15:57 -0400

    added string conversion to ast file

--- src/ast.ml
+++ src/ast.ml
@@ -44,12 +44,16 @@ type type_decl = {
 type program = expr list * func_decl list * type_decl list
 
 (* Pretty-printing functions *)
-(*
+
 let string_of_op = function
     Add -> "+"
+  | FAdd -> "+."
   | Sub -> "-"
+  | FSub -> "-."
   | Mult -> "*"
+  | FMult -> "*."
   | Div -> "/"
+  | FDiv -> "/."
   | Equal -> "=="
   | Neq -> "!="
   | Less -> "<"
@@ -59,53 +63,60 @@ let string_of_op = function
   | And -> "&&"
   | Or -> "||"
 
-let string_of_uop = function
+let string_of_preop = function 
     Neg -> "-"
   | Not -> "!"
+  | FNeg -> "-"
+  | OctaveUp -> "^"
+  | OctaveDown -> "v"
+
+let string_of_postop = function
+    Sharp -> "#"
+  | Flat -> "b"
+  | Rhythmdot -> "o"
 
 let rec string_of_expr = function
     Literal(l) -> string_of_int l
+  | FloatLit(f) -> string_of_float f
   | BoolLit(true) -> "true"
   | BoolLit(false) -> "false"
-  | Id(s) -> s
+  | ID(s) -> s
+  | String(s) -> s
   | Binop(e1, o, e2) ->
       string_of_expr e1 ^ " " ^ string_of_op o ^ " " ^ string_of_expr e2
-  | Unop(o, e) -> string_of_uop o ^ string_of_expr e
+  | Preop(o, e) -> string_of_preop o ^ string_of_expr e
+  | Postop(e, o) -> string_of_expr e ^ string_of_postop o
   | Assign(v, e) -> v ^ " = " ^ string_of_expr e
   | Call(f, el) ->
       f ^ "(" ^ String.concat ", " (List.map string_of_expr el) ^ ")"
+  | If(e1, e2, e3) -> "if " ^ string_of_expr e1 ^ " then " ^ string_of_expr e2 ^ " else " ^ string_of_expr e3
+  | Sub(e, i) -> string_of_expr e ^ "[" ^ string_of_int i ^ "]"
+  | List(es) -> "[ " ^ String.concat " " (List.map string_of_expr es) ^ " ]"
+  | PList(es) -> "p:[ " ^ String.concat " " (List.map string_of_expr es) ^ " ]"
+  | Block(es) -> "{ " ^ String.concat " " (List.map string_of_expr es) ^ " }"
+  | Concat(e1, e2) -> string_of_expr e1 ^ "@" ^ string_of_expr e2
   | Noexpr -> ""
+  | Unit -> "()"
 
-let rec string_of_stmt = function
-    Block(stmts) ->
-      "{\n" ^ String.concat "" (List.map string_of_stmt stmts) ^ "}\n"
-  | Expr(expr) -> string_of_expr expr ^ ";\n";
-  | Return(expr) -> "return " ^ string_of_expr expr ^ ";\n";
-  | If(e, s, Block([])) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s
-  | If(e, s1, s2) ->  "if (" ^ string_of_expr e ^ ")\n" ^
-      string_of_stmt s1 ^ "else\n" ^ string_of_stmt s2
-  | For(e1, e2, e3, s) ->
-      "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
-      string_of_expr e3  ^ ") " ^ string_of_stmt s
-  | While(e, s) -> "while (" ^ string_of_expr e ^ ") " ^ string_of_stmt s
 
 let string_of_typ = function
     Int -> "int"
   | Bool -> "bool"
   | Void -> "void"
+  | Float -> "float"
+  | String -> "string"
+  | Pitch -> "pitch"
 
-let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
+let string_of_func_decl fdecl =  
+  "def " ^ fdecl.ident ^ " " ^ String.concat " " fdecl.formals ^ "\n" ^ (string_of_expr fdecl.body) ^ "\n"
 
-let string_of_fdecl fdecl =
-  string_of_typ fdecl.typ ^ " " ^
-  fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
-  ")\n{\n" ^
-  String.concat "" (List.map string_of_vdecl fdecl.locals) ^
-  String.concat "" (List.map string_of_stmt fdecl.body) ^
-  "}\n"
+let string_of_type_decl tdecl = 
+  "type " ^ tdecl.typename ^ "= {\n" ^ String.concat "\n" (List.map string_of_expr tdecl.members) ^ "}\n"
 
-let string_of_program (vars, funcs) =
-  String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^
-  String.concat "\n" (List.map string_of_fdecl funcs)
 
-*)
+let string_of_program (exprs, functions, structs) =
+  String.concat "" (List.map string_of_type_decl structs) ^ "\n" ^ 
+  String.concat "" (List.map string_of_func_decl functions) ^ "\n" ^
+  String.concat "\n" (List.map string_of_expr exprs)
+
+
