423ce700ba44db332d9d09502b3897f9433412b4
habdulkafi <habdulkafi@wesleyan.edu>
2017-03-29 00:54:36 -0400

    HELLO WORLD WORKSgit statusgit status!

--- src/codegen.ml
+++ src/codegen.ml
@@ -19,7 +19,7 @@ module StringMap = Map.Make(String)
 
 let translate (exprs, functions, structs) =
   let context = L.global_context () in
-  let builder = L.builder context in
+  
   let names:(string, L.llvalue) Hashtbl.t = Hashtbl.create 10 in
   let the_module = L.create_module  context "MusicMike"
   and i32_t   = L.i32_type          context
@@ -40,10 +40,15 @@ let translate (exprs, functions, structs) =
   (* Declare printf(), which the print built-in function will call *)
   let printf_t = L.var_arg_function_type i32_t [| L.pointer_type i8_t |] in
   let printf_func = L.declare_function "printf" printf_t the_module in
+
+  let default_fun = L.define_function "main" (L.function_type (ltype_of_typ A.Void) [||]) the_module in
+  let builder = L.builder_at_end context (L.entry_block default_fun) in
+  
   let int_format_str = L.build_global_stringptr "%d\n" "fmt" builder in
+  let str_format = L.build_global_stringptr "%s\n" "str" builder in
 
   let rec expr builder = function
-      A.Literal i -> L.const_int i32_t i
+      A.Literal i ->  L.const_int i32_t i
     | A.FloatLit f -> L.const_float float_t f 
     | A.BoolLit b -> L.const_int i1_t (if b then 1 else 0)
     | A.Noexpr -> L.const_int i32_t 0
@@ -77,8 +82,10 @@ let translate (exprs, functions, structs) =
         | A.Not     -> L.build_not) e' "tmp" builder
     | A.Assign (s, e) -> let e' = expr builder e in
     ignore (L.build_store e' (lookup s) builder); e' *)
-    | A.Call ("Print", [e]) ->
+    | A.Call ("Printint", [e]) ->
        L.build_call printf_func [| int_format_str ; (expr builder e) |]  "printf" builder
+    | A.Call ("Printstr", [e]) ->
+       L.build_call printf_func [| str_format; (expr builder e) |] "printf" builder
            (* | A.Call (f, act) ->
                let (fdef, fdecl) = StringMap.find f function_decls in
   let actuals = List.rev (List.map (expr builder) (List.rev act)) in
@@ -86,5 +93,15 @@ let translate (exprs, functions, structs) =
             | _ -> f ^ "_result") in
   L.build_call fdef (Array.of_list actuals) result builder *)
     | _ -> L.const_int i32_t 1
+  in 
+    let exprbuilder builder e = ignore(expr builder e); builder
   in
+    let builder = List.fold_left exprbuilder builder exprs
+
+
+
+
+
+  in
+  ignore (L.build_ret_void builder);
   the_module
