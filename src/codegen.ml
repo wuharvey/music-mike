da614ff5a3b6354df4b5ee48bb06c845b8820baf
habdulkafi <habdulkafi@wesleyan.edu>
2017-04-10 00:01:30 -0400

    added code generation for blocks

--- src/codegen.ml
+++ src/codegen.ml
@@ -107,15 +107,18 @@ let translate (exprs, functions, structs) =
               let e' = expr builder e in 
                 ignore(L.build_store e' pointer builder)
             in
-              (* (print_string ("HEREEEEE" ^ (L.string_of_lltype (L.type_of arr_malloc)))); *) List.iteri deal_with_element es; arr_malloc
+             List.iteri deal_with_element es; arr_malloc
     | A.Subset(s, index)  ->
           let var = try Hashtbl.find main_vars s
                     with Not_found -> raise (Failure (s ^ " Not Found!"))
             in 
-              let head = (* print_string (L.string_of_llvalue var); *) L.build_load var "head" builder (* L.build_gep var [| (L.const_int i32_t index) |] "pointer" builder  *)in 
+              let head = L.build_load var "head" builder in 
               let pointer = L.build_gep head [| (L.const_int i32_t index) |] "pointer" builder in 
-              (*print_string  ("HELLO " ^(L.string_of_llvalue pointer)); *) L.build_load pointer "tmp" builder
-
+               L.build_load pointer "tmp" builder
+    | A.Block(es) -> 
+        (match es with 
+        e::e1::rest -> ignore(expr builder e); expr builder (A.Block(e1::rest))
+      | [e] -> expr builder e)
  (* | A.Unop(op, e) ->
        let e' = expr builder e in
        (match op with
