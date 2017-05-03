48690729bb874c01b7d077773487fe4605ba0f2d
Mounika <lmb2254@main-machine.c.machine-learning-146216.internal>
2017-05-03 16:47:25 +0000

    sample ap changes that aren't compiling

--- CFugue/CFugue/test/StaticLibTestApp/SampleApp.cpp
+++ CFugue/CFugue/test/StaticLibTestApp/SampleApp.cpp
@@ -1,13 +1,16 @@
 #include "stdafx.h"
 #include "stdlib.h"
 #include <iostream>
-
+#include <string>
+using namespace std;
 
 #include "CFugueLib.h"
 
 
-    int main()
+    int main(int argc, char* argv[])
     {
-        CFugue::SaveAsMidiFile(_T("Cq Dw Ex"), "MidiOutput.midi"); // Save the Music Notes to Midi file directly, without playing
+	string music(argv[1]);
+	cout << music << "\n";
+        CFugue::SaveAsMidiFile(_T(music), "MidiOutput.midi"); // Save the Music Notes to Midi file directly, without playing
         return 0;
 }
