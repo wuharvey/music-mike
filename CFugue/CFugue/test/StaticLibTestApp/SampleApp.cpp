611e202d04d4257507944642d3020691172f75a2
habdulkafi <habdulkafi@wesleyan.edu>
2017-05-03 13:37:45 -0400

    fixed up sample app so it takes cmd line args

--- CFugue/CFugue/test/StaticLibTestApp/SampleApp.cpp
+++ CFugue/CFugue/test/StaticLibTestApp/SampleApp.cpp
@@ -1,16 +1,21 @@
 #include "stdafx.h"
 #include "stdlib.h"
 #include <iostream>
-#include <string>
-using namespace std;
 
 #include "CFugueLib.h"
 
+#ifdef _UNICODE
+typedef std::wstring tstring;
+#else
+typedef std::string tstring;
+#endif
 
-    int main(int argc, char* argv[])
-    {
-	string music(argv[1]);
-	cout << music << "\n";
-        CFugue::SaveAsMidiFile(_T(music), "MidiOutput.midi"); // Save the Music Notes to Midi file directly, without playing
+
+int main(int argc, char* argv[]) {
+	std::string a(argv[1]);
+	std::cout << a << "\n";
+	std::wstring widestr = std::wstring(a.begin(), a.end());
+	const wchar_t* music = widestr.c_str();
+    CFugue::SaveAsMidiFile(music, "MidiOutput.midi"); // Save the Music Notes to Midi file directly, without playing
         return 0;
 }
