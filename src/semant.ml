bef71bb5160f4bb5b4a4035d8b13c93a40e2517a
Harvey Wu <wuharvey@gmail.com>
2017-05-08 17:19:49 -0400

    Starting semantic checker.

--- src/semant.ml
+++ src/semant.ml
@@ -4,7 +4,20 @@ open Ast
 
 module StringMap = Map.Make(String)
 
-(* Semantic checking of a program. Returns void if successful,
-   throws an exception if something is wrong. *)
+(* Hack for polymorphism compilation: take each polymorphic function and check when it is called. 
+ * Create a new Function Aexpr for each time it is called, with the specific type of the actuals. *)
 
-let check (aexprs) = ()
+let check (aexprs: aexpr list) = 
+  let is_poly ae = match ae with
+    | AFun(_,_,_,TFun(f_t, r_t)) -> 
+        let poly t = match t with 
+          | TType(_) -> true
+          | _ -> false 
+        in 
+        List.exists poly f_t 
+    | _ -> false
+  in
+  let poly = List.filter is_poly aexprs in
+  poly
+
+
