352a82b9a63f32c82616e41ebde3006de3b30780
Lakshmi Bodapati <mounikabodapati@dyn-160-39-242-117.dyn.columbia.edu>
2017-05-08 19:40:09 -0400

    need to replace Afun dummy with Afun with types using the polycalls. Struggling with syntax for mapping Acall to Afun with correct types

--- src/semant.ml
+++ src/semant.ml
@@ -1,23 +1,36 @@
 (* Semantic checking for the MicroC compiler *)
 
 open Ast
-
 module StringMap = Map.Make(String)
 
-(* Hack for polymorphism compilation: take each polymorphic function and check when it is called. 
+(* Hack for polymorphism compilation: take each polymorphic function and check when it is called.
  * Create a new Function Aexpr for each time it is called, with the specific type of the actuals. *)
 
-let check (aexprs: aexpr list) = 
+let check (aexprs: aexpr list) =
   let is_poly ae = match ae with
-    | AFun(_,_,_,TFun(f_t, r_t)) -> 
-        let poly t = match t with 
+    | AFun(_,_,_,TFun(f_t, r_t)) ->
+        let poly t = match t with
           | TType(_) -> true
-          | _ -> false 
-        in 
-        List.exists poly f_t 
+          | _ -> false
+        in
+        List.exists poly f_t
     | _ -> false
   in
   let poly = List.filter is_poly aexprs in
-  poly
+  let getname ae = match ae with
+    | AFun(fn,_,_,_) -> fn
+    | _ -> raise (Failure "What the hell you doin")
+  in
+  let poly_fnames = List.map getname poly in
 
-
+  let is_call ae = match ae with
+    | ACall(AID(fn), args, _) -> let name = fn in
+      List.mem name poly_fnames
+    | _ -> false
+  in
+  let polycalls = List.filter is_call aexprs in
+  let Afuniter ae = match ae with
+    | AFun(fn, _, _, _) -> if List.mem fn polycalls
+        then (* iterate thru polycalls for matching fnames replace AFun with new Afun with matching polycals *)
+        else (*check if function is called at all and throw away*)
+    | _ ->
