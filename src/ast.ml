1174871fca212e7174da4e0df9d6b83958ebeca5
Harvey Wu <wuharvey@gmail.com>
2017-05-10 14:23:46 -0400

    Debugging printing for Chord

--- src/ast.ml
+++ src/ast.ml
@@ -110,6 +110,7 @@ let rec string_of_expr = function
   | FloatLit(f) -> string_of_float f
   | BoolLit(true) -> "true"
   | BoolLit(false) -> "false"
+  | ChordList(_) -> "Chord"
   | ID(s) -> s
   | String(s) -> s
   | Binop(e1, o, e2) ->
