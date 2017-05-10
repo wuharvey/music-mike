f313f1607f5d93c172ffee5251b1d92d6936aaf8
Lakshmi Bodapati <mounikabodapati@dyn-160-39-242-117.dyn.columbia.edu>
2017-05-10 13:13:21 -0400

    add make_midi

--- src/lib.ml
+++ src/lib.ml
@@ -9,11 +9,10 @@ let stdlib = [
     ("Printlist", TFun([TList(TInt)], TString));
     ("Printrlist", TFun([TList(TFloat)], TString));
     ("Synth", TFun( [TList(TList(TPitch)); TList(TFloat); TList(TInt); TInt], TString));
+    ("Make_midi", TFun([TString], TUnit));
 ];;
 
-let predefined = 
-  List.fold_left (fun env (id, t) -> StringMap.add id t env) 
+let predefined =
+  List.fold_left (fun env (id, t) -> StringMap.add id t env)
   StringMap.empty stdlib
 ;;
-
-
